e9327ed04746d932ff3afbb4b3ff2d32ceb6781f
AlexStarov <alex.starov@gmail.com>
2012-12-09 23:55:36 +0200

    First root applicaton

--- proj/apps/root/admin.py
+++ proj/apps/root/admin.py
@@ -1,18 +1,2 @@
 # coding=utf-8
 __author__ = 'Админ'
-
-from proj.apps.products.models import Poll, Choice
-from django.contrib import admin
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class PollAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date'],'classes': ['collapse']})
-    ]
-    inlines = [ChoiceInline]
-
-admin.site.register(Poll, PollAdmin)
