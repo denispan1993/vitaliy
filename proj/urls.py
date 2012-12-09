e86d8f6318d4733b952995f301bd539b96fb7370
AlexStarov <alex.starov@gmail.com>
2012-12-09 23:52:27 +0200

    First root applicaton

--- proj/urls.py
+++ proj/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'proj.views.home', name='home'),
+    url(r'^$', "proj.apps.root.views.root", name='root_page'),
     # url(r'^proj/', include('proj.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
