fd1b3e3a74f9caf2353468980e48dd0e7bbcb249
AlexStarov <alex.starov@gmail.com>
2012-12-09 23:43:14 +0200

    New next Test model

--- proj/apps/products/models.py
+++ proj/apps/products/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Test(models.Model):
-    test = models.PositiveSmallIntegerField()
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+
