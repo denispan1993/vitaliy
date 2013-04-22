de123e889f6e7b228e8bd5340d53079e50108f58
AlexStarov <alex.starov@gmail.com>
2013-04-22 18:47:06 +0300

    Начало работы по странице категорий

--- apps/product/templatetags/header_category.py
+++ apps/product/templatetags/header_category.py
@@ -9,16 +9,16 @@ from django.template.loader import render_to_string
 
 register = Library()
 
-#@register.inclusion_tag('templatetags_header_category/header_category.html', )
+#@register.inclusion_tag('templatetags_header_category/header_category.jinja2.html', )
 @register.global_function()
 def header_category(current_category, ):
 
     def _rendered(current_category):
-        template_name = u'templatetags_header_category/header_category.html'
+        template_name = u'templatetags_header_category/header_category.jinja2.html'
         context ={'current_category': current_category, }
         return render_to_string(template_name=template_name, dictionary=context, )
 
     return _rendered(current_category=current_category, )
 #    return {'current_category': current_category, }
 
-# register.tag('templatetags_header_category/header_category.html', header_category, )
+# register.tag('templatetags_header_category/header_category.jinja2.html', header_category, )
