0f73e980c02d2705094b97e3b62528a1c2764da1
AlexStarov <alex.starov@gmail.com>
2013-07-19 13:00:48 +0300

    Работа над корзиной.

--- media/js/jquery/animate_cart.js
+++ media/js/jquery/animate_cart.js
@@ -5,14 +5,19 @@
  * Time: 19:07
  * To change this template use File | Settings | File Templates.
  */
+var Cart = new Object({initialize: function(){},toggle: function(){$('#cart').toggleClass('opened');}});
 
-function animate_cart (obj){
+/*function open_cart(obj){
+    $(obj).toggleClass('opened');
+};*/
+
+function animate_cart(obj){
     src = obj.src;
     left_cart = Math.ceil($(obj).offset().left);
     top_cart = Math.ceil($(obj).offset().top);
     $('<img src="'+src+'" id="temp_cart_animate" style="z-index:1000;position:absolute;top:'+top_cart+'px;left:'+left_cart+'px;">').prependTo('body');
     //$('#cart').addClass('opened');
-    $('#temp_cart_animate').animate({top: 220+$(window).scrollTop(), left: $('body').width()}, 600,
+    $('#temp_cart_animate').animate({top: 220+$(window).scrollTop(), left: $('body').width()}, 2000,
         function () {
             //$('#cart').removeClass('opened');
             $('#temp_cart_animate').remove();
