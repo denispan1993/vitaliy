175c3e0ba107ae64b80871ef1b46f4af9f98c955
AlexStarov <alex.starov@gmail.com>
2013-12-08 15:56:44 +0200

    вагон работы

--- apps/product/templatetags/header_category.py
+++ apps/product/templatetags/header_category.py
@@ -26,19 +26,22 @@ def header_category(current_category, ):
 @register.global_function()
 def header_category2(current_category, product=None, ):
 
-    root_string = u'<a href="/">Home</a>'
-    header_string = u'<a href="%s">%s</a>' % (current_category.get_absolute_url(),
-                                              current_category.title, )
+    root_string = u'<a href="/" title="Корень сайта">Home</a>'
+    header_string = u'<a href="%s" title="%s">%s</a>' % (current_category.get_absolute_url(),
+                                                         current_category.name,
+                                                         current_category.title, )
     next_category = current_category.parent
     while next_category:
-        next_category_string = u'<a href="%s">%s</a>' % (next_category.get_absolute_url(),
-                                                         next_category.title, )
+        next_category_string = u'<a href="%s" title="%s">%s</a>' % (next_category.get_absolute_url(),
+                                                                    next_category.name,
+                                                                    next_category.title, )
         header_string = u'%s&nbsp;&gt;&nbsp;%s' % (next_category_string, header_string, )
         next_category = next_category.parent
 
     _string = u'%s&nbsp;&gt;&nbsp;%s' % (root_string, header_string, )
     if product:
-        product_string = u'<a href="%s">%s</a>' % (product.get_absolute_url(),
-                                                   product.title, )
+        product_string = u'<a href="%s" title="%s">%s</a>' % (product.get_absolute_url(),
+                                                              product.name,
+                                                              product.title, )
         _string = u'%s&nbsp;&gt;&nbsp;%s' % (_string, product_string, )
     return _string
