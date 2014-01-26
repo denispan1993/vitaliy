f59cbe1392fc625a93a37b06992968beec8b872c
AlexStarov <alex.starov@gmail.com>
2014-01-26 09:20:58 +0200

    Работа над поиском. Проблема только в форме запроса ;-( POST.

--- media/js/jquery/tabSlider/TopInit.js
+++ media/js/jquery/tabSlider/TopInit.js
@@ -4,15 +4,15 @@
 try{jQuery(document).ready(function() {
         $('.auth-panel').tabSlideOut({
             tabHandle: '.auth-tab',                      //class of the element that will be your tab -doesnt have to be an anchor
-            pathToTabImage:'/media/img/feedbacktab.gif', //relative path to the image for the tab
-            imageHeight:'99px',                          //height of tab image
-            imageWidth:'30px',                           //width of tab image
+            pathToTabImage:'/media/img/feedback_top_tab.gif', //relative path to the image for the tab
+            imageHeight:'32px',                          //height of tab image
+            imageWidth:'167px',                          //width of tab image
             tabLocation:'top',                           //side of screen where tab lives, top, right, bottom, or left
-            speed:500,                                   //speed of animation
+            speed:900,                                   //speed of animation
             action:'click',                              //options: 'click' or 'hover', action to trigger animation
-            topPos:'50px',                               //position from the top/ use if tabLocation is left or right
-            leftPos:'20px',                              //position from left/ use if tabLocation is bottom or top
-            fixedPosition:false                          //options: true makes it stick(fixed position) on scroll
+            // topPos:'50px',                               //position from the top/ use if tabLocation is left or right
+            leftPos:'500px',                             //position from left/ use if tabLocation is bottom or top
+            fixedPosition:true                           //options: true makes it stick(fixed position) on scroll
         });
     })
 }catch(e){ alert(e) }
