caafb40883e6f5245b286c346979c7df00d6b3a4
AlexStarov <alex.starov@gmail.com>
2014-05-12 19:27:32 +0300

    Продолжение создания своей админ панели для редактирования заказов

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -5,11 +5,16 @@ from django.template import RequestContext
 
 def order_search(request,
                  template_name=u'order/order_search.jinja2.html', ):
-    response = render_to_response(template_name=template_name,
-#                                  dictionary={'page': page,
-#                                              'html_text': html_text, },
-                                  context_instance=RequestContext(request, ),
-                                  content_type='text/html', )
+    if request.method == 'POST':
+        POST_NAME = request.POST.get(u'POST_NAME', None, )
+        if POST_NAME == 'order_search':
+            order_id = request.POST.get(u'order_id', None, )
+    else:
+        response = render_to_response(template_name=template_name,
+    #                                  dictionary={'page': page,
+    #                                              'html_text': html_text, },
+                                      context_instance=RequestContext(request, ),
+                                      content_type='text/html', )
     # from datetime import datetime
 #    from apps.utils.datetime2rfc import datetime2rfc
 #    response['Last-Modified'] = datetime2rfc(page.updated_at, )
