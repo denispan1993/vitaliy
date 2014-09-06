d0975bd30d08d96ac2e544394b87dbedb87e43b9
AlexStarov <alex.starov@gmail.com>
2014-09-06 15:56:39 +0300

    Commit

--- apps/product/management/commands/recompyle_model_Product_field_is_availability.py
+++ apps/product/management/commands/recompyle_model_Product_field_is_availability.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand, ):
     def handle(self, *args, **options):
         from apps.product.models import Product
-        Product.objects.filter(is_availability=2, ).update(is_availability=5, )
+        # Product.objects.filter(is_availability=2, ).update(is_availability=5, )
+        # Product.objects.filter(is_availability=3, ).update(is_availability=2, )
+        # Product.objects.filter(is_availability=5, ).update(is_availability=3, )
         Product.objects.filter(is_availability=3, ).update(is_availability=2, )
-        Product.objects.filter(is_availability=5, ).update(is_availability=3, )
