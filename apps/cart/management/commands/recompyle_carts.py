4b61dde6e478f137fc4a36398692fe5604a6cb24
AlexStarov <alex.starov@gmail.com>
2014-10-05 21:48:35 +0300

    Завтра занесем на сервер. И завтра попробуем создать кучу пользователей.

--- apps/cart/management/commands/recompyle_carts.py
+++ apps/cart/management/commands/recompyle_carts.py
@@ -13,73 +13,100 @@ class Command(BaseCommand, ):
         from apps.account.models import Session_ID
         orders = Order.objects.all()
         for order in orders:
+            if 'user' in locals() or 'user' in globals():
+                del user
+
             print order
+            if order.user:
+                continue
             # first_name = order.FIO.split()[1]
             # print first_name
             last_name, first_name, patronymic = order.FIO.split()
             #last_name = last_name.stripe()
             #first_name = first_name.stripe()
             #patronymic = patronymic.stripe()
-            print first_name
-            print last_name
+#            print first_name
+#            print last_name
             # patronymic = order.FIO.split()[2]
-            print patronymic
+#            print patronymic
             email = order.email.strip()
-            print email
+#            print email
             phone = order.phone.strip().strip('-').strip('(').strip(')').lstrip('+380').lstrip('380').lstrip('80').lstrip('0')
-            print phone
-            username = ''.join(['%s' % k.capitalize() for k in last_name, first_name, patronymic], )
-            print username
+#            print phone
+#            username = ''.join(['%s' % k.capitalize() for k in last_name, first_name, patronymic], )
+#            print username
             username = ''.join(['%s' % slugify(k).capitalize() for k in last_name, first_name, patronymic], )
             # username = slugify(username, )
             print username
             sessionID = order.sessionid
             print sessionID
+
             try:
-                user = User.objects.filter(username=username, )
-            except User.DoesNotExist:
-                user = True
-            if user == []:
-                print user
-                print username
-                print u'Пользователь с таким username существует'
-                continue
+                sessionID = Session_ID.objects.get(sessionid=sessionID, )
+            except Session_ID.DoesNotExist:
+                sessionID_not_found = True
             else:
-                try:
-                    email = Email.objects.filter(email=email, )
-                except Email.DoesNotExist:
-                    email = True
-                if email == []:
-                    print email
-                    print username
-                    print u'Такой email существует у какогото пользователя'
-                    continue
+                user = sessionID.user
+
+            try:
+                email = Email.objects.get(email=email, )
+            except Email.DoesNotExist:
+                email_not_found = True
+            else:
+                if type(sessionID) != Session_ID:
+                    user = email.user
                 else:
-                    try:
-                        phone = Phone.objects.filter(phone=phone, )
-                    except Phone.DoesNotExist:
-                        phone = True
-                    if phone == []:
-                        print phone
-                        print username
-                        print u'Такой номер телефона существует у какогото пользователя'
-                        continue
+                    if email.user == sessionID.user:
+                        print u'Ok', u' - ', u'SessionID.user == Email.user'
                     else:
-                        try:
-                            sessionID = Session_ID.objects.filter(sessionid=sessionID, )
-                        except Session_ID.DoesNotExist:
-                            sessionID = True
-                        if sessionID == []:
-                            print sessionID
-                            print u'Пользователь с таким SessionID уже существует'
-                            continue
-                        else:
-                            user = User.objects.create(username=username,
-                                                       first_name=first_name,
-                                                       last_name=last_name,
-                                                       patronymic=patronymic, )
-                            email = Email.objects.create(user=user, email=email, )
-                            phone = Phone.objects.create(user=user, phone=phone, )
-                            sessionID = Session_ID.objects.create(user=user, sessionid=sessionID, )
-                            order.user = user
-                            order.save()
+                        print u'Хреновр, Email и SessionID пренадлежат разным пользователям.'
+                        print u'SessionID.user: ', sessionID.user
+                        print u'Email.user: ', email.user
+                        continue
+            try:
+                phone = Phone.objects.get(phone=phone, )
+            except Phone.DoesNotExist:
+                phone_not_found = True
+            else:
+                if type(sessionID) != Session_ID and type(email) != Email:
+                    user = phone.user
+                else:
+                    if user == phone.user:
+                        print u'Phone.user == User'
+                    else:
+                        print u'Номер телефона зарегистрирован за другим пользователем'
+                        if type(sessionID) == Session_ID:
+                            print u'SessionID.user: ', sessionID.user
+                        if type(email) == Email:
+                            print u'Email.user: ', email.user
+                        print u'Phone.user: ', phone.user
+                        continue
+
+            if 'user' not in locals() and 'user' not in globals():
+                try:
+                    user = User.objects.get(username=username, )
+                except User.DoesNotExist:
+                    user = User.objects.create(username=username,
+                                               first_name=first_name,
+                                               last_name=last_name,
+                                               patronymic=patronymic, )
+            if type(sessionID) != Session_ID:
+                sessionID = Session_ID.objects.create(user=user, sessionid=sessionID, )
+            else:
+                sessionID.user = user
+                sessionID.save()
+
+            if type(email) != Email:
+                email = Email.objects.create(user=user, email=email, )
+            else:
+                email.user = user
+                email.save()
+
+            if type(phone) != Phone:
+                phone = Phone.objects.create(user=user, phone=phone, )
+            else:
+                phone.user = user
+                phone.save()
+
+            order.user = user
+            order.save()
