79e954beb624680da21e460bce0298d48cf617fc
AlexStarov <alex.starov@gmail.com>
2014-11-30 22:10:41 +0200

    Разборки со слайдером. Замена js слайдера. Попытка № 10.

--- media/js/jquery/wowslider/script.js
+++ media/js/jquery/wowslider/script.js
@@ -50,5 +50,5 @@ var duration = 1700, delay = 4500;
 //var cSlide, bkpCont, wowInstance, timeout;
 
 //var wowSlider =
-//$("#wowslider-container1").wowSlider({effect:effects,prev:"",next:"",duration:duration,delay:delay,width:960,height:360,fullScreen:true,autoPlay:true,autoPlayVideo:false,stopOnHover:true,loop:false,bullets:true,caption:true,captionEffect:"move,slide",controls:true});
-$("#wowslider-container1").wowSlider({effect:"fly",prev:"",next:"",duration:duration,delay:delay,width:960,height:360,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:true,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+//$("#wowslider-container1").wowSlider({effect:effects,prev:"",next:"",duration:duration,delay:delay,width:960,height:360,fullScreen:true,autoPlay:true,autoPlayVideo:false,stopOnHover:true,loop:false,bullets:true,caption:true,captionEffect:"move,slide,parallax",controls:true});
+$("#wowslider-container1").wowSlider({effect:"fly",prev:"",next:"",duration:duration,delay:delay,width:960,height:360,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:true,loop:true,bullets:true,caption:true,captionEffect:"slide",controls:true,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
