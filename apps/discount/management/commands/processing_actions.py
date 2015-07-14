a76010ec9716f04fc4c397c1ed309dd9f12de884
AlexStarov <alex.starov@gmail.com>
2015-07-15 00:44:48 +0300

    Мелочевка.

--- apps/discount/management/commands/processing_actions.py
+++ apps/discount/management/commands/processing_actions.py
@@ -27,21 +27,22 @@ class Command(BaseCommand, ):
                 """
                 if action.auto_start:
                     products_of_action = action.product_in_action.not_in_action()
-                    print 'Product auto_start:', products_of_action
-                    for product in products_of_action:
-                        print 'Add product to Action: ', product
-                        """
-                            Помечает товар как учавствующий в акции
-                        """
-                        product.category.add(action_category, )
-                        product.in_action = True
-                        # """
-                        #     Меняем местами акционную нынешнюю
-                        # """
-                        # price = product.regular_price
-                        # product.regular_price = product.price
-                        # product.price = price
-                        product.save()
+                    if len(products_of_action, ) > 0:
+                        print 'Product auto_start:', products_of_action
+                        for product in products_of_action:
+                            print 'Add product to Action: ', product
+                            """
+                                Помечает товар как учавствующий в акции
+                            """
+                            product.category.add(action_category, )
+                            product.in_action = True
+                            # """
+                            #     Меняем местами акционную нынешнюю
+                            # """
+                            # price = product.regular_price
+                            # product.regular_price = product.price
+                            # product.price = price
+                            product.save()
 
         action_not_active = Action.objects.not_active()
         if action_not_active:
@@ -56,29 +57,30 @@ class Command(BaseCommand, ):
                 """
                 if action.auto_end:
                     products_of_action = action.product_in_action.in_action()
-                    print 'Product auto_end:', products_of_action
-                    for product in products_of_action:
-                        print 'Del product from Action: ', product
-                        """
-                            Помечает товар как не учавствующий в акции
-                        """
-                        product.category.remove(action_category, )
-                        product.in_action = False
-                        # """
-                        #     Меняем местами нынешнюю и акционные цены местами
-                        # """
-                        # price = product.price
-                        # product.price = product.regular_price
-                        # if action.auto_del_action_price:
-                        #     product.regular_price = 0
-                        # else:
-                        #     product.regular_price = price
-                        if action.auto_del_action_from_product:
-                            product.action.remove(action, )
-                        product.save()
-                    if action.auto_del:
-                        action.deleted = True
-                        action.save()
+                    if len(products_of_action, ) > 0:
+                        print 'Product auto_end:', products_of_action
+                        for product in products_of_action:
+                            print 'Del product from Action: ', product
+                            """
+                                Помечает товар как не учавствующий в акции
+                            """
+                            product.category.remove(action_category, )
+                            product.in_action = False
+                            # """
+                            #     Меняем местами нынешнюю и акционные цены местами
+                            # """
+                            # price = product.price
+                            # product.price = product.regular_price
+                            # if action.auto_del_action_price:
+                            #     product.regular_price = 0
+                            # else:
+                            #     product.regular_price = price
+                            if action.auto_del_action_from_product:
+                                product.action.remove(action, )
+                            product.save()
+                        if action.auto_del:
+                            action.deleted = True
+                            action.save()
             # from apps.product.models import Product
             # Product.objects.filter(is_availability=2, ).update(is_availability=5, )
             # Product.objects.filter(is_availability=3, ).update(is_availability=2, )
