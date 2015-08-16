e399d5d89ce373204e5dcca7e6bb7a9571d5d354
AlexStarov <alex.starov@gmail.com>
2015-08-16 23:23:20 +0300

    Разборки с часовым поясом.

--- apps/ajax/coupon.py
+++ apps/ajax/coupon.py
@@ -30,8 +30,12 @@ def coupon_test(request, ):
                                 'help_text': u'Странный какой-то купон', }
                 else:
                     # from datetime import datetime
-                    from django.utils import timezone
-                    now = timezone.localtime(timezone.now())
+                    from django.utils.timezone import now, localtime
+                    now = localtime(now(), )
+                    print now
+                    print now()
+                    print coupon.start_of_the_coupon
+                    print coupon.end_of_the_coupon
                     if coupon.start_of_the_coupon < now:
                         if now < coupon.end_of_the_coupon:
                             if coupon.number_of_uses < coupon.number_of_possible_uses:
