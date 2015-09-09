6cbfba56b4cea187e92ad2e557bd035f1fd7cf87
AlexStarov <alex.starov@gmail.com>
2015-09-09 09:57:27 +0300

    Недочеты

--- apps/product/templatetags/block_category.py
+++ apps/product/templatetags/block_category.py
@@ -1,20 +1,18 @@
 # coding=utf-8
 __author__ = 'Sergey'
 
-from django_jinja.library import Library
+from django_jinja.library import global_function
 from django.template.loader import render_to_string
 
-register = Library()
 
-
-@register.global_function()
+@global_function()
 def block_categories(categories, request, ):
     return render_to_string(template_name=u'category/templatetags/block_categories.jinja2.html',
                             dictionary={'categories': categories,
                                         'request': request, }, )
 
 
-@register.global_function()
+@global_function()
 def block_category(category, cycle, ):
     if cycle == 1:
         category_block_margin = '0 0 10px 0px;'
@@ -25,7 +23,7 @@ def block_category(category, cycle, ):
                                         'category_block_margin': category_block_margin, }, )
 
 
-@register.global_function()
+@global_function()
 def this_category(category, width_this_category, ):
     # print(width_this_category)
     return render_to_string(template_name=u'category/templatetags/this_category.jinja2.html',
