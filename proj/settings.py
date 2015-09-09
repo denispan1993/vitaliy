3bbaf7040ecca5b093df9913c619c60dba6f5e62
AlexStarov <alex.starov@gmail.com>
2015-09-09 12:47:47 +0300

    Cтранная ошибка. Возвращаемся к стандартному методу показа категорий.

--- proj/settings.py
+++ proj/settings.py
@@ -141,6 +141,8 @@ USE_I18N = True
 # calendars according to the current locale.
 USE_L10N = True
 
+DEFAULT_CHARSET = 'utf-8'
+DEFAULT_CONTENT_TYPE = 'text/html'
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 #MEDIA_ROOT = 'C:/Python27/Lib/site-packages/django/contrib/admin'
