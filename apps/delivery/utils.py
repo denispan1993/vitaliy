7db66f48f60bfdef197b294620c0beddeb647c64
AlexStarov <alex.starov@gmail.com>
2015-10-11 23:36:21 +0300

    Фигня какая-то

--- apps/delivery/utils.py
+++ apps/delivery/utils.py
@@ -14,10 +14,12 @@ def parsing(value, key, ):
     if part_count > 1:
         value = ''
         for value_part in values[::2]:  # перечисляем все куски с шагом 2
-            print value_part.encode('utf8', )
-            print values[n+1].encode('utf8', )
+            print 'value_part: ', value_part.encode('utf8', )
+            print 'values[n+1]: ', values[n+1].encode('utf8', )
+            print 'value: ', value.encode('utf8', )
+            print 'n: ', n, ' cycle: ', cycle
             value = '%s%s%s%s' % (value, value_part, key, values[n+1], )
+            print 'value: ', value.encode('utf8', )
             n += 2
             cycle += 1
-            print 'n: ', n, ' cycle: ', cycle
     return value
