64f401fd651ffabe7b2dc65cc913241c525414ae
AlexStarov <alex.starov@gmail.com>
2015-10-11 23:58:48 +0300

    Фигня какая-то

--- apps/delivery/utils.py
+++ apps/delivery/utils.py
@@ -7,25 +7,22 @@ def parsing(value, key, ):
     values = split("{{ id }}*", value, )
     print value.encode('utf8', )
     print values[0].encode('utf8', )
-    n = 1
+    n = 0
     cycle = 0
     part_count = len(values, )
     print 'part_count: ', part_count
     if part_count > 1:
         value = ''
         for value_part in values[::2]:  # перечисляем все куски с шагом 2
-            if not n % 2 == 0:
-                value1 = values[n]
-                value2 = values[n+1]
-            else:
-                value1 = values[n-1]
-                value2 = values[n]
-            print 'value_part: ', value1.encode('utf8', )
-            print 'values[n+1]: ', value2.encode('utf8', )
+            #if n % 2 == 0:
+            value1 = values[cycle]
+            value = '%s%s%s' % (value, value1, key, )
+            #else:
+                #value1 = values[cycle]
+                #value = '%s%s%s' % (value, value1, key, )
+            print 'value1: ', value1.encode('utf8', )
             print 'value: ', value.encode('utf8', )
             print 'n: ', n, ' cycle: ', cycle
-            value = '%s%s%s%s' % (value, value1, key, value2, )
-            print 'value: ', value.encode('utf8', )
             n += 2
             cycle += 1
         return value
