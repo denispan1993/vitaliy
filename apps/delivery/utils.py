028855b7f587257d4bf989589f23521523ba16ed
AlexStarov <alex.starov@gmail.com>
2015-10-12 00:17:24 +0300

    Фигня какая-то

--- apps/delivery/utils.py
+++ apps/delivery/utils.py
@@ -5,13 +5,15 @@ __author__ = 'AlexStarov'
 def parsing(value, key, ):
     from re import split
     values = split("{{ id }}*", value, )
-    cycle = 0
+    cycle = 1
     part_count = len(values, )
     if part_count > 1:
         value = ''
         for value_part in values:  # [::2]:  # перечисляем все куски с шагом 2
-            value = '%s%s%s' % (value, value_part, key, )
-            cycle += 1
-            if part_count == cycle+1:
+            if not part_count == cycle+1:
+                value = '%s%s%s' % (value, value_part, key, )
+            else:
+                value = '%s%s' % (value, value_part, )
                 break
+            cycle += 1
     return value
