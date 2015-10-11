bb75efb8281ef8aee5993d388b2b4a4b57149dfe
AlexStarov <alex.starov@gmail.com>
2015-10-11 22:13:22 +0300

    Фигня какая-то

--- apps/delivery/utils.py
+++ apps/delivery/utils.py
@@ -8,10 +8,11 @@ def parsing(value, key, ):
     cycle = 0
     value = ''
     part_count = len(values, )
-    if part_count > 0:
+    print 'part_count: ', part_count
+    if part_count > 1:
         for value_part in values[::2]:  # перечисляем все куски с шагом 2
+            value = '%s%s%s%s' % (value, value_part, key, values[n+1], )
             n += 2
             cycle += 1
             print 'n: ', n, ' cycle: ', cycle
-            value = '%s%s%s%s' % (value, value_part, key, values[n], )
     return value
