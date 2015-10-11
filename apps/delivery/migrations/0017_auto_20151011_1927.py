2fc4a7ed6c6e915f2fd6501468fca378e6373059
AlexStarov <alex.starov@gmail.com>
2015-10-11 20:30:08 +0300

    Email instance

--- apps/delivery/migrations/0017_auto_20151011_1927.py
+++ apps/delivery/migrations/0017_auto_20151011_1927.py
@@ -7,8 +7,11 @@ import datetime
 
 class Migration(migrations.Migration):
 
+    #dependencies = [
+    #    ('delivery', '0016_auto_20151006_1601'),
+    #]
     dependencies = [
-        ('delivery', '0016_auto_20151006_1601'),
+        ('delivery', '0015_auto_20151006_1558'),
     ]
 
     operations = [
