a20a24f7530cb44ba366ff006f80ec01758f3aa3
AlexStarov <alex.starov@gmail.com>
2015-10-15 13:57:31 +0300

    Lобавил запись url при переадресации

---
+++ apps/delivery/migrations/0024_auto_20151015_1357.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('delivery', '0023_auto_20151011_2202'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='traceofvisits',
+            name='url',
+            field=models.CharField(max_length=255, null=True, verbose_name='URL \u043f\u0435\u0440\u0435\u0430\u0434\u0440\u0435\u0441\u0430\u0446\u0438\u0438', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='delivery',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 831228), auto_now_add=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='delivery',
+            name='name',
+            field=models.CharField(default=b'2015-10-15T13:57:06.830995', max_length=128, null=True, verbose_name='\u0418\u043c\u044f \u0440\u0430\u0441\u0441\u044b\u043b\u043a\u0438', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='delivery',
+            name='updated_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 831267), auto_now=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailfordelivery',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 840568), auto_now_add=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailfordelivery',
+            name='key',
+            field=models.CharField(unique=True, max_length=8, verbose_name='ID E-Mail \u0430\u0434\u0440\u0435\u0441\u0430 \u0438 \u0440\u0430\u0441\u0441\u044b\u043b\u043a\u0438'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailfordelivery',
+            name='updated_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 840611), auto_now=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailmiddledelivery',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 832443), auto_now_add=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailmiddledelivery',
+            name='updated_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 832483), auto_now=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='mailaccount',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 830166), auto_now_add=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='mailaccount',
+            name='updated_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 830209), auto_now=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='traceofvisits',
+            name='created_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 15, 13, 57, 6, 841573), auto_now_add=True, null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u044f'),
+            preserve_default=True,
+        ),
+    ]
