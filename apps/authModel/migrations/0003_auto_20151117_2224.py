0e6987352494901b695010689e7eb75093099ee5
AlexStarov <alex.starov@gmail.com>
2015-11-17 22:25:41 +0200

    Попытка добавить возможность добавления "безымянных" E-Mail'ов

---
+++ apps/authModel/migrations/0003_auto_20151117_2224.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.auth.models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authModel', '0002_email_bad_email'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='user',
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='email',
+            field=models.EmailField(max_length=254, verbose_name='email address'),
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='user',
+            field=models.ForeignKey(related_name='email_parent_user', verbose_name='\u041f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044c', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='last_login',
+            field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
+        ),
+    ]
