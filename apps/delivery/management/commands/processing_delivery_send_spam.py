c7f1c16e7288ea7a5cae7d39adc6cb1422418314
AlexStarov <alex.starov@gmail.com>
2015-11-23 17:27:07 +0200

    Развернутый ответ по ексепшину

--- apps/delivery/management/commands/processing_delivery_send_spam.py
+++ apps/delivery/management/commands/processing_delivery_send_spam.py
@@ -107,6 +107,10 @@ class Command(BaseCommand, ):
                                     time += 30
                                 except SMTPDataError as e:
                                     print 'SMTPDataError: ', e
+                                    print 'SMTPDataError: smtp_code', e.smtp_code
+                                    print 'SMTPDataError: smtp_error', e.smtp_error
+                                    print 'SMTPDataError: args', e.args
+                                    print 'SMTPDataError: message', e.message
                                     if "5.7.1 Message rejected under suspicion of SPAM;" in e:
                                         print 'SPAM Bloked E-Mail: ', mail_account, ' NOW !!!!!!!!!!!!!!!!!!!!!!!'
                                         from datetime import datetime
