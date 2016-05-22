fda3e5d0720129543a5857c64b7570ad9d3c26b3
AlexStarov <alex.starov@gmail.com>
2016-05-22 20:24:40 +0300

    logging

--- proj/processor/request_Middleware.py
+++ proj/processor/request_Middleware.py
@@ -17,12 +17,12 @@ class Process_Request_Middleware(object):
         try:
             logging_log_info.info('self = {0}'.format(self))
             logging_log_info.info('self.__class__ = {0}'.format(self.__class__))
-            logging_log_info.info('self.__doc__ = {0}'.format(self.__doc__))
+            #logging_log_info.info('self.__doc__ = {0}'.format(self.__doc__))
             logging_log_info.info('self.__module__= {0}'.format(self.__module__))
-            logging_log_info.info('self.__repr__() = {0}'.format(self.__repr__()))
+            #logging_log_info.info('self.__repr__() = {0}'.format(self.__repr__()))
             logging_log_info.info('object = {0}'.format(object))
             logging_log_info.info('object.__class__ = {0}'.format(object.__class__))
-            logging_log_info.info('object.__doc__ = {0}'.format(object.__doc__))
+            #logging_log_info.info('object.__doc__ = {0}'.format(object.__doc__))
             logging_log_info.info('object.__module__ = {0}'.format(object.__module__))
             # logging_log_info.info('object.__repr__() = {0}'.format(object.__repr__()))
         except Exception as e:
