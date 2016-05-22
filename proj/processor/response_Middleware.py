5eca4849c91718301678a9c0ac8bea6958141e24
AlexStarov <alex.starov@gmail.com>
2016-05-22 22:27:50 +0300

    logging

--- proj/processor/response_Middleware.py
+++ proj/processor/response_Middleware.py
@@ -6,10 +6,15 @@ __author__ = 'AlexStarov'
 
 logging = getLogger(__name__)
 
+skip_url = ('/ajax/resolution/', '/ajax/cookie/', '/ajax/timezone/client/', '/ajax/geoip/resolve/', )
+
 
 class Process_Response_Middleware(object):
 
     def process_response(self, request, response):
-        logging.info(u'Stop executions (END): {0}'.format(time.time()))
+        full_path = request.path
+
+        if full_path not in skip_url:
+            logging.info(u'Stop executions (END): {0}'.format(time.time()))
 
         return response
