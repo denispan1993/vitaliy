95a10a1512ecede7ae81db0efe2f0017c01b51d7
AlexStarov <alex.starov@gmail.com>
2016-05-22 20:44:07 +0300

    logging

--- proj/processor/request_Middleware.py
+++ proj/processor/request_Middleware.py
@@ -14,19 +14,19 @@ class Process_Request_Middleware(object):
 
     def process_request(self, request, ):
         logging_log_info.info(u'')
-        try:
-            logging_log_info.info('self = {0}'.format(self))
-            logging_log_info.info('self.__class__ = {0}'.format(self.__class__))
-            # logging_log_info.info('self.__doc__ = {0}'.format(self.__doc__))
-            logging_log_info.info('self.__module__= {0}'.format(self.__module__))
-            logging_log_info.info('self.__repr__() = {0}'.format(self.__repr__()))
-            logging_log_info.info('object = {0}'.format(object))
-            logging_log_info.info('object.__class__ = {0}'.format(object.__class__))
-            logging_log_info.info('object.__doc__ = {0}'.format(object.__doc__))
-            logging_log_info.info('object.__module__ = {0}'.format(object.__module__))
-            # logging_log_info.info('object.__repr__() = {0}'.format(object.__repr__()))
-        except Exception as e:
-            logging_log_error.error('Exception: {0}'.format(e))
+        #try:
+        #    logging_log_info.info('self = {0}'.format(self))
+        #    logging_log_info.info('self.__class__ = {0}'.format(self.__class__))
+        #    # logging_log_info.info('self.__doc__ = {0}'.format(self.__doc__))
+        #    logging_log_info.info('self.__module__= {0}'.format(self.__module__))
+        #    logging_log_info.info('self.__repr__() = {0}'.format(self.__repr__()))
+        #    logging_log_info.info('object = {0}'.format(object))
+        #    logging_log_info.info('object.__class__ = {0}'.format(object.__class__))
+        #    logging_log_info.info('object.__doc__ = {0}'.format(object.__doc__))
+        #    logging_log_info.info('object.__module__ = {0}'.format(object.__module__))
+        #    # logging_log_info.info('object.__repr__() = {0}'.format(object.__repr__()))
+        #except Exception as e:
+        #    logging_log_error.error('Exception: {0}'.format(e))
 
         logging_log_info.info(u'Start executions (START): {0}'.format(time.time()))
 
