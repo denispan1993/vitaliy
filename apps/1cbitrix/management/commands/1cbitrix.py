5cf6ed55e9e11f392a1bab6ad5f494550d6388c9
AlexStarov <alex.starov@gmail.com>
2016-06-10 17:49:21 +0300

    1cbitrix

--- apps/1cbitrix/management/commands/1cbitrix.py
+++ apps/1cbitrix/management/commands/1cbitrix.py
@@ -11,36 +11,33 @@ __author__ = 'AlexStarov'
 
 def search_in_category(name, id_1c, parent=None, ):
     try:
-        if parent:
-            return Category.objects.get(title__icontains=name, parent=parent)
-        else:
-            return Category.objects.get(title__icontains=name)
+        cat = Category.objects.get(title=name, id_1c=id_1c, parent=parent)
+        return cat
     except Category.DoesNotExist:
         cat = Category()
         cat.title = name
         if parent:
             cat.parent = parent
         cat.id_1c = id_1c
-        cat.save()
-        return cat
+        # cat.save()
+        return None
     except Category.MultipleObjectsReturned:
-        if parent:
-            cats = Category.objects.filter(title=name, parent=parent)
-            if len(cats) > 1:
-                raise 'MultiCat'
-            elif len(cats) == 1:
-                return cats[0]
-            elif len(cats) == 0:
-                try:
-                    cat = Category.objects.get(title=name, parent=parent)
-                except Category.DoesNotExist:
-                    cat = Category()
-                    cat.title = name
-                    if parent:
-                        cat.parent = parent
-                    cat.id_1c = id_1c
-                    cat.save()
-                    return cat
+        cats = Category.objects.filter(title=name, id_1c=id_1c, parent=parent)
+        if len(cats) > 1:
+            raise 'MultiCat'
+        elif len(cats) == 1:
+            return cats[0]
+        elif len(cats) == 0:
+            try:
+                cat = Category.objects.get(title=name, parent=parent)
+            except Category.DoesNotExist:
+                cat = Category()
+                cat.title = name
+                if parent:
+                    cat.parent = parent
+                cat.id_1c = id_1c
+                # cat.save()
+            return None
 
 
 class Command(BaseCommand, ):
@@ -114,7 +111,7 @@ class Command(BaseCommand, ):
                                                                 dict_elem_level6 = {'Id': elem_level6.text, 'Name': elems_level5[elem_level6_Indx + 1].text, }
 
                                                                 parent_cat6 = search_in_category(name=dict_elem_level6['Name'], id_1c=dict_elem_level6['Id'])
-                                                                print 'level6: ', dict_elem_level6, parent_cat6
+                                                                #print 'level6: ', dict_elem_level6, parent_cat6
 
                                                             if elem_level6.tag == u'Группы':
                                                                 elems_level6 = list(elems_level5[elem_level6_Indx])
@@ -132,7 +129,7 @@ class Command(BaseCommand, ):
                                                                                     dict_elem_level8 = {'Id': elem_level8.text, 'Name': elems_level7[elem_level8_Indx + 1].text, }
 
                                                                                     parent_cat8 = search_in_category(name=dict_elem_level8['Name'], id_1c=dict_elem_level8['Id'], parent=parent_cat6)
-                                                                                    print 'level6: ', dict_elem_level6, parent_cat8
+                                                                                    #print 'level6: ', dict_elem_level6, parent_cat8
 
                                                                                 if elem_level8.tag == u'Группы':
                                                                                     elems_level8 = list(elems_level7[elem_level8_Indx])
@@ -150,7 +147,7 @@ class Command(BaseCommand, ):
                                                                                                         dict_elem_level10 = {'Id': elem_level10.text, 'Name': elems_level9[elem_level10_Indx + 1].text, }
 
                                                                                                         parent_cat10 = search_in_category(name=dict_elem_level10['Name'], id_1c=dict_elem_level10['Id'], parent=parent_cat8)
-                                                                                                        print 'level6: ', dict_elem_level6, parent_cat10
+                                                                                                        #print 'level6: ', dict_elem_level6, parent_cat10
 
                                                                                                     if elem_level10.tag == u'Группы':
                                                                                                         level10 = True
@@ -162,6 +159,46 @@ class Command(BaseCommand, ):
 
                                                     except IndexError:
                                                         pass
+
+                    if elem_level1.tag == u'Каталог':
+
+                        elems_level1 = list(elem_level1)
+
+                        for elem_level2_Indx, elem_level2 in enumerate(elems_level1):
+                            print 'level2', elem_level2_Indx, elem_level2, elem_level2.tag, elem_level2.attrib, elem_level2.text
+
+                            if elem_level2.tag == u'Наименование' \
+                                    and elem_level2.text == u'Каталог товаров' \
+                                    and elems_level1[elem_level2_Indx + 1].tag == u'Товары':
+
+                                elems_level2 = list(elems_level1[elem_level2_Indx + 1])
+
+                                for elem_level3_Indx, elem_level3 in enumerate(elems_level2):
+                                    # print 'level3', elem_level3_Indx, elem_level3, elem_level3.tag, elem_level3.attrib, elem_level3.text
+
+                                    if elem_level3.tag == u'Товар':
+
+                                        elems_level3 = list(elem_level3)
+
+                                        for elem_level4_Indx, elem_level4 in enumerate(elems_level3):
+                                            # print 'level4', elem_level4_Indx, elem_level4, elem_level4.tag, elem_level4.attrib, elem_level4.text
+
+                                            if elem_level4.tag == u'Ид':
+                                                id_1c_prod = elem_level4.text
+                                            if elems_level3[elem_level4_Indx + 1].tag == u'Артикул':
+                                                articul = elems_level3[elem_level4_Indx + 1].text
+                                            if elems_level3[elem_level4_Indx + 2].tag == u'Наименование':
+                                                name = elems_level3[elem_level4_Indx + 2].text
+
+                                            if elem_level4.tag == u'Группы':
+
+                                                elems_level4 = list(elems_level3[elem_level4_Indx])
+
+                                                for elem_level5_Indx, elem_level5 in enumerate(elems_level4):
+                                                    # print 'level5', elem_level5_Indx, elem_level5, elem_level5.tag, elem_level5.attrib, elem_level5.text
+
+                                                    if elem_level5.tag == u'Ид':
+                                                        id_1c_cat = elem_level5.text
 
         if 'level10' in locals():
             print 'level10'
