4e804548be12b78d3af79db1af103d8562b9d2aa
AlexStarov <alex.starov@gmail.com>
2016-06-13 14:57:35 +0300

    celery

--- bin/serv_celery_beat.sh
+++ bin/serv_celery_beat.sh
@@ -5,7 +5,6 @@ cd ..
 ./manage-serv.py celery beat --app=proj \
 --scheduler='djcelery.schedulers.DatabaseScheduler' \
 --detach \
---events \
 --loglevel=DEBUG \
 --logfile=../../logs/keksik_com_ua/celery/beat.log \
 --pidfile=../../run/celery__keksik_com_ua__beat.pid
