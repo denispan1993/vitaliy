28123972f29076b0d24f6e07501eb001e10a095d
AlexStarov <alex.starov@gmail.com>
2016-06-13 13:36:55 +0300

    celery

--- bin/serv_celery_beat.sh
+++ bin/serv_celery_beat.sh
@@ -1,7 +1,7 @@
 #!/www/envs/keksik_com_ua/bin/python
 # -*- coding: utf-8 -*-
 
-celery beat --app=proj \
+/www/envs/keksik_com_ua/bin/python celery beat --app=proj \
 --scheduler='djcelery.schedulers.DatabaseScheduler' \
 --loglevel=DEBUG \
 --logfile=../../logs/keksik_com_ua/celery/beat.log \
