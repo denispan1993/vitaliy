a39df2e74bb7143be09d3ec232d283769e3330ed
AlexStarov <alex.starov@gmail.com>
2016-06-17 23:45:30 +0300

    delivery fix

--- apps/delivery/utils.py
+++ apps/delivery/utils.py
@@ -239,7 +239,7 @@ def create_msg(delivery, mail_account, email, exception=False, test=False, ):
     headers = {'Reply-To': formataddr((u'Интернет магаизн Keksik', mail_account.email)), }
 
     message_kwargs = {
-        'subject': 'test - {}'.format(delivery.subject) if test else delivery.subject,
+        'subject': u'test - {}'.format(delivery.subject) if test else delivery.subject,
         'body': strip_tags(parsing(value=delivery.html, key=email.key, ), ),
         'headers': headers,
         'from_email': formataddr((u'Интернет магаизн Keksik', mail_account.email)),
