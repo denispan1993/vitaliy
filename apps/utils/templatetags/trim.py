8f3cf34fcf8d24dd532a8be1d9c66d025588bef0
AlexStarov <alex.starov@gmail.com>
2016-08-18 16:59:19 +0300

    Мелочи

--- apps/utils/templatetags/trim.py
+++ apps/utils/templatetags/trim.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-__author__ = 'AlexStarov'
-
 from django_jinja.library import filter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+import re
+
+__author__ = 'AlexStarov'
 
 
 @filter(name='trim_whitespace', )
 def trim(value, ):
     return value.strip()
+
+
+@filter(name='spacify', )
+def spacify(value, autoescape=None):
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    return mark_safe(re.sub('\s', '&'+'nbsp;', esc(value)))
+
+spacify.needs_autoescape = True
