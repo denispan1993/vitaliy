9e8a640653c20117e2b801b40d25f4f4f9cf7cca
AlexStarov <alex.starov@gmail.com>
2016-09-25 13:02:56 +0300

    Socks

---
+++ bin/serv__celery__queue__socks_server_get__worker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd ..
+
+./manage-serv.py celery worker --app=proj \
+--concurrency=1 \
+--autoreload \
+--queues=queues__socks_server_get \
+--hostname=queues__socks_server_get__worker \
+--detach \
+--loglevel=DEBUG \
+--logfile=../../logs/keksik_com_ua/celery/queues__socks_server_get__worker.log \
+--pidfile=../../run/celery__keksik_com_ua__queue_socks_server_get__worker.pid &
+
+
+
+# %h: Hostname including domain name.
+# %n: Hostname only.
+# %d: Domain name only.
