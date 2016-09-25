81b280d5ce76a7ee142c111bc16ce8637dcb5be2
AlexStarov <alex.starov@gmail.com>
2016-09-25 13:37:27 +0300

    Socks fix

--- bin/serv__celery__queue__socks_server_get__worker.sh
+++ bin/serv__celery__queue__socks_server_get__worker.sh
@@ -7,11 +7,11 @@ cd ..
 --autoreload \
 --queues=queues__socks_server_get \
 --hostname=queues__socks_server_get__worker \
+--detach \
 --loglevel=DEBUG \
 --logfile=../../logs/keksik_com_ua/celery/queues__socks_server_get__worker.log \
 --pidfile=../../run/celery__keksik_com_ua__queue_socks_server_get__worker.pid &
 
-#--detach \
 
 
 # %h: Hostname including domain name.
