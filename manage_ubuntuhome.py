d5a9d0edb4514d896c3150b4ef578945337468de
AlexStarov <alex.starov@gmail.com>
2016-10-08 15:41:01 +0300

    requirements

--- manage_ubuntuhome.py
+++ manage_ubuntuhome.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 __author__ = 'AlexStarov'
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "proj.settings"
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
