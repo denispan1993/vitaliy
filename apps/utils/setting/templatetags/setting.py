88ffe12eddf20024c746aea441c2a06ba120e312
AlexStarov <alex.starov@gmail.com>
2016-10-26 23:59:50 +0300

    Make Responsive Adaptive version

--- apps/utils/setting/templatetags/setting.py
+++ apps/utils/setting/templatetags/setting.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django_jinja.library import global_function
 
+from apps.utils.setting.models import Setting
+
 __author__ = 'AlexStarov'
 
 variable_types = {'char', 'text', 'integer', 'positivesmallinteger', 'img', }
@@ -10,30 +12,29 @@ variable_types = {'char', 'text', 'integer', 'positivesmallinteger', 'img', }
 def get_value_variable(variable_name, variable_type, ):
     value = 'None'
     variable_type = variable_type.lower()
-    from apps.utils.setting.models import Setting
     try:
         variable_set = Setting.objects.get(variable_name=variable_name, )
-    except Setting.DoesNotExist:
-        variable_set = None
-        value = 'value of %s variable_name, variable_type is %s is None' % (variable_name, variable_type, )
-    else:
-        if variable_type == 'img':
+
+        if variable_type is 'img':
             """ Получаем запись непосредственно на запись в базе данных на нашу картинку """
-            string = 'variable_set.%s.all()[0]' % variable_type
-            value = eval(string,
+
+            value = eval('variable_set.%s.all()[0]' % variable_type,
                          {'__builtins__': {}, },
                          {'variable_set': variable_set, }, )
+
             return value.photo,\
                    value.photo.path,\
                    value.photo.url
-                   # value.photo.url.split('/media', 1, )[1]  # Почему-то путь выводится с дополнительным /media
-                   #                                          # нам нужно его обрезать.
+
         if variable_type in variable_types:
-            #string = "value = variable_set.%s" % variable_type
-            #exec string
+
             string = 'variable_set.%s' % variable_type
-            #  eval("(lambda:0).func_code", {'__builtins__':{}})
-            value = eval(string,
-                         {'__builtins__': {}, },
-                         {'variable_set': variable_set, }, )
-    return value
+            return eval(string,
+                        {'__builtins__': {}, },
+                        {'variable_set': variable_set, }, )
+
+    except Setting.DoesNotExist:
+        if variable_type is 'img':
+            return None
+        else:
+            return 'value of %s variable_name, variable_type is %s is None' % (variable_name, variable_type, )
