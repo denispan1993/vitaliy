68a977fcd95fdfe35a2132c1c515dae37a8c43c6
AlexStarov <alex.starov@gmail.com>
2016-12-12 20:55:44 +0200

    celery --> supervisor

--- bin/serv_celery_queue_celery.sh
+++ bin/serv_celery_queue_celery.sh
@@ -4,13 +4,14 @@
 --concurrency=1 \
 --autoreload \
 --queues=celery \
---hostname=queue_celery__worker \
---detach \
+--hostname=queue_celery \
 --loglevel=DEBUG \
---logfile=../../logs/keksik_com_ua/celery/queue_celery_worker.log \
---pidfile=../../run/celery__keksik_com_ua__queue_celery__worker.pid &
+--logfile=../../logs/keksik_com_ua/celery/queue_celery.log \
+--pidfile=../../run/celery__keksik_com_ua__queue_celery.pid &
 
 
+
+#--detach \
 
 # %h: Hostname including domain name.
 # %n: Hostname only.
