f2361f1cbf58045043ec2ce19f83f68a740b1525
AlexStarov <alex.starov@gmail.com>
2016-12-12 21:05:56 +0200

    celery --> supervisor

--- bin/serv_celery_queue_celery.sh
+++ bin/serv_celery_queue_celery.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-/www/envs/keksik_com_ua/bin/python celery worker --app=proj \
+/www/envs/keksik_com_ua/manage-serv.py celery worker --app=proj \
 --concurrency=1 \
 --autoreload \
 --queues=celery \
 --hostname=queue_celery \
 --loglevel=DEBUG \
 --logfile=../../logs/keksik_com_ua/celery/queue_celery.log \
---pidfile=../../run/celery__keksik_com_ua__queue_celery.pid &
+--pidfile=../../run/celery__keksik_com_ua__queue_celery.pid
 
 
 
