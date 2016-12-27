8d9dc236ac841026fae9d5a4d88ed42156df7b18
AlexStarov <alex.starov@gmail.com>
2016-12-27 23:10:36 +0200

    admin SMS

--- apps/sms_ussd/admin.py
+++ apps/sms_ussd/admin.py
@@ -6,7 +6,9 @@ __author__ = 'AlexStarov'
 
 
 class SMSAdmin(admin.ModelAdmin, ):
-    pass
+    list_display = ['pk', 'template', 'direction', 'user', 'sessionid', 'task-id',
+                    'is_send', 'from_phone_char', 'to_phone_char', ]
+    list_display_links = ['pk', 'direction', 'user', 'sessionid', 'task-id', ]
 
 admin.site.register(SMS, SMSAdmin, )
 
