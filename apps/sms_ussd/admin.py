9c7e842301a12b10cbb4feb6584eeea065d37905
AlexStarov <alex.starov@gmail.com>
2016-12-27 23:42:10 +0200

    admin SMS

--- apps/sms_ussd/admin.py
+++ apps/sms_ussd/admin.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import SMS, Template
+from .models import SMS, Template, USSD
 
 __author__ = 'AlexStarov'
 
 
 class SMSAdmin(admin.ModelAdmin, ):
-    list_display = ['pk', 'template', 'direction', 'user', 'sessionid', 'task-id',
+    list_display = ['pk', 'template', 'direction', 'user', 'sessionid', 'task_id',
                     'is_send', 'from_phone_char', 'to_phone_char', ]
-    list_display_links = ['pk', 'direction', 'user', 'sessionid', 'task-id', ]
+    list_display_links = ['pk', 'direction', 'user', 'sessionid', 'task_id',
+                          'is_send', 'from_phone_char', 'to_phone_char', ]
 
 admin.site.register(SMS, SMSAdmin, )
 
@@ -17,3 +18,12 @@ class TemplateAdmin(admin.ModelAdmin, ):
     pass
 
 admin.site.register(Template, TemplateAdmin, )
+
+
+class USSDAdmin(admin.ModelAdmin, ):
+    list_display = ['pk', 'from_device', 'direction', 'user', 'sessionid', 'task_id',
+                    'is_send', 'from_phone_char', 'to_phone_char', ]
+    list_display_links = ['pk', 'from_device', 'direction', 'user', 'sessionid', 'task_id',
+                          'is_send', 'from_phone_char', 'to_phone_char', ]
+
+admin.site.register(USSD, USSDAdmin, )
