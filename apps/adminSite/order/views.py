65739d8d2fb1ffb0e692a942c9ee7121a89869d8
AlexStarov <alex.starov@gmail.com>
2016-12-29 16:36:32 +0200

    SMS USSD

--- apps/adminSite/order/views.py
+++ apps/adminSite/order/views.py
@@ -1,80 +1,65 @@
 # -*- coding: utf-8 -*-
-__author__ = 'AlexStarov'
-
+from datetime import datetime, timedelta
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from apps.cart.models import Order
+
+__author__ = 'AlexStarov'
 
 
 @staff_member_required
 def order_search(request,
                  template_name=u'order/order_search.jinja2', ):
+
     error_message = u''
-    if request.method == 'POST':
-        POST_NAME = request.POST.get(u'POST_NAME', None, )
-        if POST_NAME == 'order_search':
+
+    if request.method == 'POST' and request.POST.get(u'POST_NAME', False, ) == 'order_search':
+
             order_id = request.POST.get(u'order_id', None, )
             if order_id:
                 try:
                     order_id = int(order_id, )
-                except ValueError:
-                    error_message = u'Некорректно введен номер заказа.'
-                else:
-                    from apps.cart.models import Order
+
                     try:
-                        order = Order.objects.get(pk=order_id, )
+                        Order.objects.get(pk=order_id, )
+                        return redirect(to='order_edit', id='%06d' % order_id, )
                     except Order.DoesNotExist:
                         error_message = u'Заказа с таким номером не существует.'
-                    else:
-                        order_id = '%06d' % order_id
-                        from django.shortcuts import redirect
-                        return redirect(to='order_edit', id=order_id, )
-    from datetime import datetime, timedelta
-    filter_datetime = datetime.now() - timedelta(days=31, )
-    from apps.cart.models import Order
+                except ValueError:
+                    error_message = u'Некорректно введен номер заказа.'
+
+    filter_datetime = datetime.now() - timedelta(days=15, )
     orders = Order.objects.filter(created_at__gte=filter_datetime, )
-    response = render(request=request,
-                      template_name=template_name,
-                      context={'error_message': error_message,
-                               'orders': orders, },
-                      content_type='text/html', )
-    return response
+    return render(request=request,
+                  template_name=template_name,
+                  context={'error_message': error_message,
+                           'orders': orders, },
+                  content_type='text/html', )
 
 
 @staff_member_required
 def order_edit_product_add(request,
                            order_id,
                            template_name=u'order/order_edit_product_add.jinja2', ):
-    from django.shortcuts import redirect
     if order_id:
         try:
             order_id = int(order_id, )
-        except ValueError:
-            error_message = u'Некорректно введен номер заказа.'
-            return redirect(to='order_search', )
-        else:
-            from apps.cart.models import Order
+
             try:
                 order = Order.objects.get(pk=order_id, )
             except Order.DoesNotExist:
-                error_message = u'В базе отсутсвует заказ с таким номером.'
+                ''' В базе отсутсвует заказ с таким номером '''
                 return redirect(to='order_search', )
-            else:
-                pass
+
+        except ValueError:
+            ''' Некорректно введен номер заказа '''
+            return redirect(to='order_search', )
+
     else:
-        error_message = u'Отсутсвует номер заказа.'
+        ''' Отсутсвует номер заказа '''
         return redirect(to='order_search', )
 
-#    from apps.static.models import Static
-#    try:
-#        page = Static.objects.get(url=static_page_url, )
-#    except Static.DoesNotExist:
-#        from django.http import Http404
-#        raise Http404
-#    import markdown
-#    if page:
-#        html_text = markdown.markdown(page.text, )
-#    else:
-#        html_text = None
     return render(request=request,
                   template_name=template_name,
                   context={'order_id': order_id,
@@ -86,35 +71,62 @@ def order_edit_product_add(request,
 def order_edit(request,
                order_id,
                template_name=u'order/order_edit.jinja2', ):
-    from django.shortcuts import redirect
-    if order_id:
+
+    if not order_id:
+        ''' Отсутсвует номер заказа '''
+        return redirect(to='order_search', )
+    else:
+
         try:
             order_id = int(order_id, )
-        except ValueError:
-            error_message = u'Некорректно введен номер заказа.'
-            return redirect(to='order_search', )
-        else:
-            from apps.cart.models import Order
+
             try:
                 order = Order.objects.get(pk=order_id, )
             except Order.DoesNotExist:
-                error_message = u'В базе отсутсвует заказ с таким номером.'
+                ''' В базе отсутсвует заказ с таким номером '''
                 return redirect(to='order_search', )
-    else:
-        error_message = u'Отсутсвует номер заказа.'
-        return redirect(to='order_search', )
 
-#    from apps.static.models import Static
-#    try:
-#        page = Static.objects.get(url=static_page_url, )
-#    except Static.DoesNotExist:
-#        from django.http import Http404
-#        raise Http404
-#    import markdown
-#    if page:
-#        html_text = markdown.markdown(page.text, )
-#    else:
-#        html_text = None
+        except ValueError:
+            ''' Некорректно введен номер заказа '''
+            return redirect(to='order_search', )
+
+    if request.method == 'POST' and request.POST.get(u'POST_NAME', False, ) == 'order_dispatch':
+
+        order_pk = request.POST.get(u'order_pk', None, )
+        if order_pk:
+            try:
+                order_pk = int(order_pk, )
+
+                if order_pk == order_id:
+                    ''' Отправка e-mail письма и SMS с суммой и реквизитами на оплату '''
+
+                    #delivery_order.apply_async(
+                    #    queue='delivery_send',
+                    #    kwargs={'order_pk': order.pk, },
+                    #    task_id='celery-task-id-delivery_order-{0}'.format(celery.utils.uuid(), ),
+                    #)
+
+                    phone = order.phone \
+                        .lstrip('+') \
+                        .replace('(', '').replace(')', '').replace(' ', '') \
+                        .replace('-', '').replace('.', '').replace(',', '') \
+                        .lstrip('380').lstrip('38').lstrip('80').lstrip('0')
+
+                    if len(phone, ) == 9:
+                        send_template_sms.apply_async(
+                            queue='delivery_send',
+                            kwargs={
+                                'sms_to_phone_char': '+380%s' % phone[:9],
+                                'sms_template_name': proj.settings.SMS_TEMPLATE_NAME['SEND_ORDER_NUMBER'],
+                                'sms_order_number': order.pk,
+                            },
+                            task_id='celery-task-id-send_template_sms-{0}'.format(celery.utils.uuid(), ),
+                        )
+
+            except ValueError:
+                ''' Некорректно введен номер заказа '''
+                return redirect(to='order_search', )
+
     return render(request=request,
                   template_name=template_name,
                   context={'order_id': order_id,
