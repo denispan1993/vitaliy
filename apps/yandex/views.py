c63af327e1d9d1bd1b226e043008e9b9d15ad797
AlexStarov <alex.starov@gmail.com>
2017-04-21 13:51:50 +0300

    yandex shop.yml

--- apps/yandex/views.py
+++ apps/yandex/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import View
 from django.utils.importlib import import_module
 
 from datetime import datetime
+import time
 
 from lxml import etree
 
@@ -29,11 +30,14 @@ class GenerateShopYMLView(View):
         for currency in YML_CONFIG['currencies']:
             etree.SubElement(currencies, 'currency', rate=currency['rate'], id=currency['id'])
 
+        start_time = time.time()
         self.set_categories(shop)
-
+        print(" self.set_categories(shop) --- %s seconds ---" % (time.time() - start_time))
         # etree.SubElement(shop, 'local_delivery_cost').text = YML_CONFIG['local_delivery_cost']
 
-        # self.set_products(shop)
+        start_time = time.time()
+        self.set_products(shop)
+        print(" self.set_products(shop) --- %s seconds ---" % (time.time() - start_time))
 
         # print etree.tostring(root)
 
@@ -46,7 +50,7 @@ class GenerateShopYMLView(View):
         mod = import_module(class_module)
         clazz = getattr(mod, class_name)
         categories_tag = etree.SubElement(shop, 'categories')
-        for category in clazz.objects.all().values('id', 'parent_id', 'title', ).order_by('id'):
+        for category in clazz.objects.published().values('id', 'parent_id', 'title', ).order_by('id'):
             if not category['parent_id']:
                 etree.SubElement(categories_tag,
                                  'category',
@@ -60,18 +64,19 @@ class GenerateShopYMLView(View):
     def set_products(self, shop):
         offers = etree.SubElement(shop, 'offers')
         i = 0
-        for product in Product.objects.filter(is_active=True):
-            offer = etree.SubElement(offers, 'offer', id=str(product.id), available="true")
+        for product in Product.objects.published().values('id', 'parent_id', 'title', ).order_by('id'):
+            offer = etree.SubElement(offers, 'offer', id=str(product['id']), available="true")
             etree.SubElement(offer, 'url').text = YML_CONFIG['url'] + product.get_absolute_url()
             etree.SubElement(offer, 'price').text = str(product.get_price())
             # etree.SubElement(offer, 'currencyId').text = product.get_currency()
             try:
-                etree.SubElement(offer, 'categoryId').text = str(product.category.all()[0].id)
+                etree.SubElement(offer, 'categoryId').text =\
+                    str(product.category.all().values_list('id', flat=True)[0])
             except IndexError:
                 pass
                 # etree.SubElement(offer, 'picture').text = YML_CONFIG['url'] + product.head_image.url
             etree.SubElement(offer, 'delivery').text = "true"
-            etree.SubElement(offer, 'name').text = product.title
+            etree.SubElement(offer, 'name').text = product['title']
             #etree.SubElement(offer, 'name').text = product.get_name()
             i += 1
 
