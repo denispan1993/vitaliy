24e42e5796d6952d010eddb0c62567f27f452f4c
AlexStarov <alex.starov@gmail.com>
2017-06-29 00:27:55 +0300

    fix task

--- applications/yml/tasks.py
+++ applications/yml/tasks.py
@@ -112,5 +112,5 @@ def generate_prom_ua_yml(*args, **kwargs):
     set_products(shop)
     print(" self.set_products(shop) --- %s seconds ---" % (time.time() - start_time), len(db.connection.queries))
 
-    with open('storage/yml/prom.ua/shop.xml', 'w') as f:
+    with open('/www/projs/prod.keksik_com_ua/storage/yml/prom.ua/shop.xml', 'w') as f:
         f.write(etree.tostring(root))
